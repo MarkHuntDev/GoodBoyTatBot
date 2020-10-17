b1fe34648baed85c67fe1b91a0e7959c47564e7e
Rinat Zalyaletdinov <rinat.zalyaletdinov@yandex.com>
2020-10-17 12:05:34 +0300

    Integrate checkstyle
    1. Integrate maven-checkstyle-plugin
    http://maven.apache.org/plugins/maven-checkstyle-plugin/index.html
    
    2. Fit sources under checkstyle rules
    
    Resolves: #3

--- src/main/java/tatbash/telegram/TelegramBot.java
+++ src/main/java/tatbash/telegram/TelegramBot.java
@@ -1,5 +1,6 @@
 package tatbash.telegram;
 
+import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -11,40 +12,38 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import tatbash.infrastructure.config.TelegramBotProperties;
 
-import javax.annotation.PostConstruct;
-
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
-    private final TelegramBotProperties properties;
+  private final TelegramBotProperties properties;
 
-    @PostConstruct
-    public void selfRegister() throws TelegramApiRequestException {
-        new TelegramBotsApi().registerBot(this);
-    }
+  @PostConstruct
+  public void selfRegister() throws TelegramApiRequestException {
+    new TelegramBotsApi().registerBot(this);
+  }
 
-    @Override
-    public void onUpdateReceived(Update update) {
-        try {
-            SendMessage response = new SendMessage();
-            response.setChatId(update.getMessage().getChatId());
-            response.setText(update.getMessage().getText());
-            execute(response);
-        } catch (TelegramApiException ex) {
-            ex.printStackTrace();
-            log.error(ex.getLocalizedMessage(), ex);
-        }
+  @Override
+  public void onUpdateReceived(Update update) {
+    try {
+      SendMessage response = new SendMessage();
+      response.setChatId(update.getMessage().getChatId());
+      response.setText(update.getMessage().getText());
+      execute(response);
+    } catch (TelegramApiException ex) {
+      ex.printStackTrace();
+      log.error(ex.getLocalizedMessage(), ex);
     }
+  }
 
-    @Override
-    public String getBotUsername() {
-        return this.properties.getName();
-    }
+  @Override
+  public String getBotUsername() {
+    return this.properties.getName();
+  }
 
-    @Override
-    public String getBotToken() {
-        return this.properties.getToken();
-    }
+  @Override
+  public String getBotToken() {
+    return this.properties.getToken();
+  }
 }
