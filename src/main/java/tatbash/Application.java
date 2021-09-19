f800789a3a786633c22ec7a9a994066ce54a3022
Rinat Zalyaletdinov <rinat.zalyaletdinov@yandex.com>
2021-09-19 14:15:56 +0500

    Bump JDK version
    According to bumping of java version it's required to bump
    some library versions and configure GitHub Action.

--- src/main/java/tatbash/Application.java
+++ src/main/java/tatbash/Application.java
@@ -3,13 +3,11 @@ package tatbash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.telegram.telegrambots.ApiContextInitializer;
 
 @EnableConfigurationProperties
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
-    ApiContextInitializer.init();
     SpringApplication.run(Application.class, args);
   }
 }
--- src/main/java/tatbash/infrastructure/telegram/TelegramBot.java
+++ src/main/java/tatbash/infrastructure/telegram/TelegramBot.java
@@ -1,7 +1,8 @@
-package tatbash.telegram;
+package tatbash.infrastructure.telegram;
 
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -10,6 +11,7 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import tatbash.infrastructure.config.TelegramBotProperties;
 
 @Slf4j
@@ -19,22 +21,19 @@ public class TelegramBot extends TelegramLongPollingBot {
 
   private final TelegramBotProperties properties;
 
-  @PostConstruct
-  public void selfRegister() throws TelegramApiRequestException {
-    new TelegramBotsApi().registerBot(this);
+  @Override
+  @SneakyThrows(TelegramApiException.class)
+  public void onUpdateReceived(Update update) {
+    SendMessage response = new SendMessage();
+    response.setChatId(update.getMessage().getChatId().toString());
+    response.setText(update.getMessage().getText());
+    execute(response);
   }
 
-  @Override
-  public void onUpdateReceived(Update update) {
-    try {
-      SendMessage response = new SendMessage();
-      response.setChatId(update.getMessage().getChatId());
-      response.setText(update.getMessage().getText());
-      execute(response);
-    } catch (TelegramApiException ex) {
-      ex.printStackTrace();
-      log.error(ex.getLocalizedMessage(), ex);
-    }
+  @PostConstruct
+  @SneakyThrows({TelegramApiRequestException.class, TelegramApiException.class})
+  public void selfRegister() {
+    new TelegramBotsApi(DefaultBotSession.class).registerBot(this);
   }
 
   @Override
