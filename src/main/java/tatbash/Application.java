9f46a14bcc5de6dc5986917b03c206bee99faae2
Rinat Zalyaletdinov <rinat.zalyaletdinov@yandex.com>
2021-09-19 15:51:45 +0500

    Test CI

--- src/main/java/tatbash/Application.java
+++ src/main/java/tatbash/Application.java
@@ -8,6 +8,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+       SpringApplication.run(Application.class, args);
   }
 }
