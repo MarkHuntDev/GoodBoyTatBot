61775fc2f24190dbdd6cddc585d9a62182076b01
Rinat Zalyaletdinov <rinat.zalyaletdinov@yandex.com>
2022-01-12 22:34:55 +0300

    Create MessageIn class

---
+++ src/test/java/tatbash/telegram/MessageInTest.java
@@ -0,0 +1,68 @@
+package tatbash.telegram;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import org.junit.jupiter.api.Test;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+class MessageInTest {
+
+  @Test
+  void hasMessage_should_return_false_when_update_does_not_contain_message_text() {
+    // when:
+    final var whenUpdateIsNull = MessageIn
+        .builder()
+        .from(null)
+        .build();
+
+    final var whenMessageIsNull = MessageIn
+        .builder()
+        .from(new Update())
+        .build();
+
+    final var whenMessageTextIsNull = MessageIn
+        .builder()
+        .from(messageWithNullText())
+        .build();
+
+    // then:
+    assertThat(whenUpdateIsNull.hasMessage()).isFalse();
+    assertThat(whenUpdateIsNull.getText()).isNull();
+
+    assertThat(whenMessageIsNull.hasMessage()).isFalse();
+    assertThat(whenMessageIsNull.getText()).isNull();
+
+    assertThat(whenMessageTextIsNull.hasMessage()).isFalse();
+    assertThat(whenMessageTextIsNull.getText()).isNull();
+  }
+
+  @Test
+  void hasMessage_should_return_true_when_update_contains_message_text() {
+    // when:
+    final var messageWithNotNullText = MessageIn
+        .builder()
+        .from(messageWithNotNullText())
+        .build();
+
+    // then:
+    assertThat(messageWithNotNullText.hasMessage()).isTrue();
+    assertThat(messageWithNotNullText.getText()).isNotNull();
+    assertThat(messageWithNotNullText.getText()).isEmpty();
+  }
+
+  private Update messageWithNullText() {
+    final var update = new Update();
+    update.setMessage(new Message());
+    return update;
+  }
+
+  private Update messageWithNotNullText() {
+    final var update = new Update();
+    final var message = new Message();
+    message.setText("");
+    update.setMessage(message);
+    return update;
+  }
+}
