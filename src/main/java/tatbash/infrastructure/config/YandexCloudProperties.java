cab668a92df211464313710608d39526ea272ada
Rinat Zalyaletdinov <rinat.zalyaletdinov@yandex.com>
2022-01-14 22:01:41 +0300

    Polish

--- src/main/java/tatbash/infrastructure/config/YandexCloudProperties.java
+++ src/main/java/tatbash/infrastructure/config/YandexCloudProperties.java
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConstructorBinding
-@ConfigurationProperties(prefix = "yandex.cloud")
+@ConfigurationProperties("yandex.cloud")
 public record YandexCloudProperties(String folderId, String authorizationToken) {
 }
