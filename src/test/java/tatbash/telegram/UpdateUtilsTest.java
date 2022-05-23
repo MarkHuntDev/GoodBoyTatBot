ab4ed7cde5896b474e94a933beeeeb5ce3c7dcfe
Rinat Zalyaletdinov <rinat.zalyaletdinov@yandex.com>
2022-05-24 00:42:08 +0300

    Add new utility methods to UpdateUtils

--- src/test/java/tatbash/telegram/UpdateUtilsTest.java
+++ src/test/java/tatbash/telegram/UpdateUtilsTest.java
@@ -3,8 +3,12 @@ package tatbash.telegram;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static tatbash.telegram.UpdateUtils.extractChatId;
+import static tatbash.telegram.UpdateUtils.extractHashtags;
+import static tatbash.telegram.UpdateUtils.extractText;
 import static tatbash.telegram.UpdateUtils.messageTextExists;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,6 +16,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 class UpdateUtilsTest {
@@ -55,6 +60,96 @@ class UpdateUtilsTest {
   }
 
   @Test
+  void should_return_extracted_text() {
+    final var expectedText = "test text";
+    final var actualChatId = extractText(
+        new HandyUpdate()
+            .setMessage(
+                new HandyMessage()
+                    .setText(expectedText)
+                    .message()
+            )
+            .update()
+    );
+    assertThat(actualChatId).isEqualTo(expectedText);
+  }
+
+  @ParameterizedTest
+  @MethodSource("candidatesWithAbsentTextAndWithErrorMessage")
+  void should_throw_exception_when_update_does_not_contain_text(Update update, String message) {
+    assertThatThrownBy(() -> extractText(update))
+        .isInstanceOf(NullPointerException.class)
+        .hasMessage(message);
+  }
+
+  private static Stream<Arguments> candidatesWithAbsentTextAndWithErrorMessage() {
+    return Stream.of(
+        Arguments.of(null, "update object can't be null"),
+        Arguments.of(
+            new HandyUpdate()
+                .setMessage(null)
+                .update(), "message object can't be null"
+        ),
+        Arguments.of(
+            new HandyUpdate()
+                .setMessage(
+                    new HandyMessage()
+                        .setText(null)
+                        .message()
+                )
+                .update(), "message text can't be null"
+        )
+    );
+  }
+
+  @Test
+  void should_return_extracted_hashtags() {
+    final var expectedHashtag1 = "#hashtag1";
+    final var expectedHashtag2 = "#hashtag2";
+    final var expectedHashtags = List.of(expectedHashtag1, expectedHashtag2);
+    final var actualHashtags = extractHashtags(
+        new HandyUpdate()
+            .setMessage(
+                new HandyMessage()
+                    .addMessageEntity(
+                        new HandyMessageEntity()
+                            .setType("hashtag")
+                            .setText(expectedHashtag1)
+                            .entity()
+                    )
+                    .addMessageEntity(
+                        new HandyMessageEntity()
+                            .setType("hashtag")
+                            .setText(expectedHashtag2)
+                            .entity()
+                    )
+                    .message()
+            )
+            .update()
+    );
+    assertThat(actualHashtags).isEqualTo(expectedHashtags);
+  }
+
+  @ParameterizedTest
+  @MethodSource("candidatesWithAbsentMessageAndWithErrorMessage")
+  void should_throw_exception_when_update_does_not_contain_message(Update update, String message) {
+    assertThatThrownBy(() -> extractHashtags(update))
+        .isInstanceOf(NullPointerException.class)
+        .hasMessage(message);
+  }
+
+  private static Stream<Arguments> candidatesWithAbsentMessageAndWithErrorMessage() {
+    return Stream.of(
+        Arguments.of(null, "update object can't be null"),
+        Arguments.of(
+            new HandyUpdate()
+                .setMessage(null)
+                .update(), "message object can't be null"
+        )
+    );
+  }
+
+  @Test
   void should_return_true_when_update_contains_text() {
     assertThat(messageTextExists(
         new HandyUpdate()
@@ -69,12 +164,12 @@ class UpdateUtilsTest {
   }
 
   @ParameterizedTest
-  @MethodSource("candidatesWithAbsentText")
+  @MethodSource("candidatesWithAbsentTextAndWithoutErrorMessage")
   void should_return_false_when_update_does_not_contain_text(Update update) {
     assertThat(messageTextExists(update)).isFalse();
   }
 
-  private static Stream<Arguments> candidatesWithAbsentText() {
+  private static Stream<Arguments> candidatesWithAbsentTextAndWithoutErrorMessage() {
     return Stream.of(
         Arguments.of((Update) null),
         Arguments.of(
@@ -111,6 +206,7 @@ class UpdateUtilsTest {
   private static class HandyMessage {
 
     private final Message message = new Message();
+    private final List<MessageEntity> entities = new ArrayList<>();
 
     HandyMessage setText(String text) {
       this.message.setText(text);
@@ -122,7 +218,13 @@ class UpdateUtilsTest {
       return this;
     }
 
+    HandyMessage addMessageEntity(MessageEntity entity) {
+      this.entities.add(entity);
+      return this;
+    }
+
     Message message() {
+      this.message.setEntities(new ArrayList<>(this.entities));
       return this.message;
     }
   }
@@ -138,6 +240,25 @@ class UpdateUtilsTest {
 
     Chat chat() {
       return this.chat;
+    }
+  }
+
+  private static class HandyMessageEntity {
+
+    private final MessageEntity entity = new MessageEntity();
+
+    HandyMessageEntity setType(String type) {
+      this.entity.setType(type);
+      return this;
+    }
+
+    HandyMessageEntity setText(String text) {
+      this.entity.setText(text);
+      return this;
+    }
+
+    MessageEntity entity() {
+      return this.entity;
     }
   }
 }
