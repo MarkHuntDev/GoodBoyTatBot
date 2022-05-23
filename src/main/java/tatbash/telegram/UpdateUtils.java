7dee16a5cb07fce06f104ca603972a14d1b16e14
Rinat Zalyaletdinov <rinat.zalyaletdinov@yandex.com>
2022-05-24 00:42:08 +0300

    Add new utility methods to UpdateUtils

--- src/main/java/tatbash/telegram/UpdateUtils.java
+++ src/main/java/tatbash/telegram/UpdateUtils.java
@@ -1,13 +1,36 @@
 package tatbash.telegram;
 
+import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
 
+import java.util.List;
+import java.util.Objects;
 import lombok.experimental.UtilityClass;
+import org.springframework.util.CollectionUtils;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @UtilityClass
 class UpdateUtils {
+
+  static List<String> extractHashtags(Update update) {
+    final var message = nonNullMessage(update);
+    if (CollectionUtils.isEmpty(message.getEntities())) {
+      return emptyList();
+    }
+    return message.getEntities()
+        .stream()
+        .filter(Objects::nonNull)
+        .filter(UpdateUtils::isHashtag)
+        .map(MessageEntity::getText)
+        .collect(toList());
+  }
+
+  private static boolean isHashtag(MessageEntity entity) {
+    return "hashtag".equals(entity.getType());
+  }
 
   static boolean messageTextExists(Update update) {
     return update != null
@@ -17,6 +40,10 @@ class UpdateUtils {
 
   static Long extractChatId(Update update) {
     return nonNullMessage(update).getChatId();
+  }
+
+  static String extractText(Update update) {
+    return requireNonNull(nonNullMessage(update).getText(), "message text can't be null");
   }
 
   private static Message nonNullMessage(Update update) {
