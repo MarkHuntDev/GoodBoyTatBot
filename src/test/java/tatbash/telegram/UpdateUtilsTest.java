74a965527213536f538e27619f77eb4a0b143ad0
Rinat Zalyaletdinov <rinat.zalyaletdinov@yandex.com>
2022-06-06 23:59:32 +0300

    Extract builder classes into utility class
    Also introduced some likeness to BDD in unit-tests.

--- src/test/java/tatbash/telegram/UpdateUtilsTest.java
+++ src/test/java/tatbash/telegram/UpdateUtilsTest.java
@@ -7,37 +7,42 @@ import static tatbash.telegram.UpdateUtils.extractHashtags;
 import static tatbash.telegram.UpdateUtils.extractText;
 import static tatbash.telegram.UpdateUtils.messageTextExists;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.telegram.telegrambots.meta.api.objects.Chat;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import tatbash.telegram.FixtureUtils.ChatBuilder;
+import tatbash.telegram.FixtureUtils.MessageBuilder;
+import tatbash.telegram.FixtureUtils.MessageEntityBuilder;
+import tatbash.telegram.FixtureUtils.UpdateBuilder;
 
 class UpdateUtilsTest {
 
   @Test
   void should_return_extracted_chat_id() {
+    // given:
     final var expectedChatId = 999L;
-    final var actualChatId = extractChatId(
-        new HandyUpdate()
-            .setMessage(
-                new HandyMessage()
-                    .setChat(
-                        new HandyChat()
-                            .setId(expectedChatId)
-                            .chat()
-                    )
-                    .message()
-            )
-            .update()
-    );
-    assertThat(actualChatId).isEqualTo(expectedChatId);
+    final var expectedUpdate = new UpdateBuilder()
+        .setMessage(
+            new MessageBuilder()
+                .setChat(
+                    new ChatBuilder()
+                        .setId(expectedChatId)
+                        .build()
+                )
+                .build()
+        )
+        .build();
+
+    // when:
+    final var actualChatId = extractChatId(expectedUpdate);
+
+    // then:
+    assertThat(actualChatId)
+        .isEqualTo(expectedChatId);
   }
 
   @ParameterizedTest
@@ -50,28 +55,37 @@ class UpdateUtilsTest {
 
   private static Stream<Arguments> candidatesWithAbsentChatId() {
     return Stream.of(
-        Arguments.of(null, "update object can't be null"),
         Arguments.of(
-            new HandyUpdate()
+            null,
+            "update object can't be null"
+        ),
+        Arguments.of(
+            new UpdateBuilder()
                 .setMessage(null)
-                .update(), "message object can't be null"
+                .build(),
+            "message object can't be null"
         )
     );
   }
 
   @Test
   void should_return_extracted_text() {
+    // given:
     final var expectedText = "test text";
-    final var actualChatId = extractText(
-        new HandyUpdate()
-            .setMessage(
-                new HandyMessage()
-                    .setText(expectedText)
-                    .message()
-            )
-            .update()
-    );
-    assertThat(actualChatId).isEqualTo(expectedText);
+    final var expectedUpdate = new UpdateBuilder()
+        .setMessage(
+            new MessageBuilder()
+                .setText(expectedText)
+                .build()
+        )
+        .build();
+
+    // when:
+    final var actualChatId = extractText(expectedUpdate);
+
+    // then:
+    assertThat(actualChatId)
+        .isEqualTo(expectedText);
   }
 
   @ParameterizedTest
@@ -84,50 +98,58 @@ class UpdateUtilsTest {
 
   private static Stream<Arguments> candidatesWithAbsentTextAndWithErrorMessage() {
     return Stream.of(
-        Arguments.of(null, "update object can't be null"),
         Arguments.of(
-            new HandyUpdate()
-                .setMessage(null)
-                .update(), "message object can't be null"
+            null,
+            "update object can't be null"
         ),
         Arguments.of(
-            new HandyUpdate()
+            new UpdateBuilder()
+                .setMessage(null)
+                .build(),
+            "message object can't be null"
+        ),
+        Arguments.of(
+            new UpdateBuilder()
                 .setMessage(
-                    new HandyMessage()
+                    new MessageBuilder()
                         .setText(null)
-                        .message()
+                        .build()
                 )
-                .update(), "message text can't be null"
+                .build(),
+            "message text can't be null"
         )
     );
   }
 
   @Test
   void should_return_extracted_hashtags() {
+    // given:
     final var expectedHashtag1 = "#hashtag1";
     final var expectedHashtag2 = "#hashtag2";
     final var expectedHashtags = List.of(expectedHashtag1, expectedHashtag2);
-    final var actualHashtags = extractHashtags(
-        new HandyUpdate()
-            .setMessage(
-                new HandyMessage()
-                    .addMessageEntity(
-                        new HandyMessageEntity()
-                            .setType("hashtag")
-                            .setText(expectedHashtag1)
-                            .entity()
-                    )
-                    .addMessageEntity(
-                        new HandyMessageEntity()
-                            .setType("hashtag")
-                            .setText(expectedHashtag2)
-                            .entity()
-                    )
-                    .message()
-            )
-            .update()
-    );
-    assertThat(actualHashtags).isEqualTo(expectedHashtags);
+    final var expectedEntityMessage1 = new MessageEntityBuilder()
+        .setType("hashtag")
+        .setText(expectedHashtag1)
+        .build();
+    final var expectedEntityMessage2 = new MessageEntityBuilder()
+        .setType("hashtag")
+        .setText(expectedHashtag2)
+        .build();
+    final var expectedUpdate = new UpdateBuilder()
+        .setMessage(
+            new MessageBuilder()
+                .addMessageEntity(expectedEntityMessage1)
+                .addMessageEntity(expectedEntityMessage2)
+                .build()
+        )
+        .build();
+
+    // when:
+    final var actualHashtags = extractHashtags(expectedUpdate);
+
+    // then:
+    assertThat(actualHashtags)
+        .isEqualTo(expectedHashtags);
   }
 
   @ParameterizedTest
@@ -140,125 +162,67 @@ class UpdateUtilsTest {
 
   private static Stream<Arguments> candidatesWithAbsentMessageAndWithErrorMessage() {
     return Stream.of(
-        Arguments.of(null, "update object can't be null"),
         Arguments.of(
-            new HandyUpdate()
+            null,
+            "update object can't be null"
+        ),
+        Arguments.of(
+            new UpdateBuilder()
                 .setMessage(null)
-                .update(), "message object can't be null"
+                .build(),
+            "message object can't be null"
         )
     );
   }
 
   @Test
   void should_return_true_when_update_contains_text() {
-    assertThat(messageTextExists(
-        new HandyUpdate()
-            .setMessage(
-                new HandyMessage()
-                    .setText("")
-                    .message()
-            )
-            .update()
-    )).isTrue();
+    // given:
+    final var expectedUpdate = new UpdateBuilder()
+        .setMessage(
+            new MessageBuilder()
+                .setText("")
+                .build()
+        )
+        .build();
+
+    // when:
+    final var exists = messageTextExists(expectedUpdate);
+
+    // then:
+    assertThat(exists)
+        .isTrue();
 
   }
 
   @ParameterizedTest
   @MethodSource("candidatesWithAbsentTextAndWithoutErrorMessage")
   void should_return_false_when_update_does_not_contain_text(Update update) {
-    assertThat(messageTextExists(update)).isFalse();
+    // when:
+    final var exists = messageTextExists(update);
+
+    // then:
+    assertThat(exists)
+        .isFalse();
   }
 
   private static Stream<Arguments> candidatesWithAbsentTextAndWithoutErrorMessage() {
     return Stream.of(
         Arguments.of((Update) null),
         Arguments.of(
-            new HandyUpdate()
+            new UpdateBuilder()
                 .setMessage(null)
-                .update()
+                .build()
         ),
         Arguments.of(
-            new HandyUpdate()
+            new UpdateBuilder()
                 .setMessage(
-                    new HandyMessage()
+                    new MessageBuilder()
                         .setText(null)
-                        .message()
+                        .build()
                 )
-                .update()
+                .build()
         )
     );
-  }
-
-  private static class HandyUpdate {
-
-    private final Update update = new Update();
-
-    HandyUpdate setMessage(Message message) {
-      this.update.setMessage(message);
-      return this;
-    }
-
-    Update update() {
-      return this.update;
-    }
-  }
-
-  private static class HandyMessage {
-
-    private final Message message = new Message();
-    private final List<MessageEntity> entities = new ArrayList<>();
-
-    HandyMessage setText(String text) {
-      this.message.setText(text);
-      return this;
-    }
-
-    HandyMessage setChat(Chat chat) {
-      this.message.setChat(chat);
-      return this;
-    }
-
-    HandyMessage addMessageEntity(MessageEntity entity) {
-      this.entities.add(entity);
-      return this;
-    }
-
-    Message message() {
-      this.message.setEntities(new ArrayList<>(this.entities));
-      return this.message;
-    }
-  }
-
-  private static class HandyChat {
-
-    private final Chat chat = new Chat();
-
-    HandyChat setId(Long id) {
-      this.chat.setId(id);
-      return this;
-    }
-
-    Chat chat() {
-      return this.chat;
-    }
-  }
-
-  private static class HandyMessageEntity {
-
-    private final MessageEntity entity = new MessageEntity();
-
-    HandyMessageEntity setType(String type) {
-      this.entity.setType(type);
-      return this;
-    }
-
-    HandyMessageEntity setText(String text) {
-      this.entity.setText(text);
-      return this;
-    }
-
-    MessageEntity entity() {
-      return this.entity;
-    }
   }
 }
