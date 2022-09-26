7dee2efdb567e024466d2f9e0f9bcb0363b4ae01
Rinat Zalyaletdinov <rinat.zalyaletdinov@yandex.com>
2022-09-27 00:22:23 +0300

    Add translation service url to YandexCloudProperties

--- src/main/java/tatbash/infrastructure/config/YandexCloudProperties.java
+++ src/main/java/tatbash/infrastructure/config/YandexCloudProperties.java
@@ -5,12 +5,18 @@ import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConstructorBinding
 @ConfigurationProperties("yandex.cloud")
-public record YandexCloudProperties(String folderId, String authorizationToken) {
+public record YandexCloudProperties(String translationUrl, String folderId, String authorizationToken) {
 
   /**
    * Params {@code folderId} and {@code authorizationToken} can't be null or empty.
    */
   public YandexCloudProperties {
+    if (translationUrl == null) {
+      throw new NullPointerException("translationUrl can't be null");
+    }
+    if (translationUrl.isBlank()) {
+      throw new IllegalArgumentException("translationUrl can't be empty");
+    }
     if (folderId == null) {
       throw new NullPointerException("folderId can't be null");
     }
--- src/test/java/tatbash/infrastructure/config/YandexCloudPropertiesTest.java
+++ src/test/java/tatbash/infrastructure/config/YandexCloudPropertiesTest.java
@@ -13,32 +13,35 @@ class YandexCloudPropertiesTest {
 
   @ParameterizedTest
   @MethodSource("illegalParamsCandidates")
-  void should_throw_exception_when_any_param_is_illegal(String folderId,
+  void should_throw_exception_when_any_param_is_illegal(String translationUrl,
+                                                        String folderId,
                                                         String authorizationToken,
                                                         Class<RuntimeException> exceptionClass,
                                                         String message) {
-    assertThatThrownBy(() -> new YandexCloudProperties(folderId, authorizationToken))
+    assertThatThrownBy(() -> new YandexCloudProperties(translationUrl, folderId, authorizationToken))
         .isInstanceOf(exceptionClass)
         .hasMessage(message);
   }
 
   private static Stream<Arguments> illegalParamsCandidates() {
     return Stream.of(
-        Arguments.of(null, "x", NullPointerException.class, "folderId can't be null"),
-        Arguments.of("x", null, NullPointerException.class, "authorizationToken can't be null"),
-        Arguments.of("", "x", IllegalArgumentException.class, "folderId can't be empty"),
-        Arguments.of("x", "", IllegalArgumentException.class, "authorizationToken can't be empty"),
-        Arguments.of(" ", "x", IllegalArgumentException.class, "folderId can't be empty"),
-        Arguments.of("x", " ", IllegalArgumentException.class, "authorizationToken can't be empty")
+        Arguments.of(null, "x", "x", NullPointerException.class, "translationUrl can't be null"),
+        Arguments.of("x", null, "x", NullPointerException.class, "folderId can't be null"),
+        Arguments.of("x", "x", null, NullPointerException.class, "authorizationToken can't be null"),
+        Arguments.of("", "x", "x", IllegalArgumentException.class, "translationUrl can't be empty"),
+        Arguments.of("x", "", "x", IllegalArgumentException.class, "folderId can't be empty"),
+        Arguments.of("x", "x", "", IllegalArgumentException.class, "authorizationToken can't be empty")
     );
   }
 
   @Test
   void should_create_an_instance_successfully() {
-    final var actual = new YandexCloudProperties("x", "y");
-    assertThat(actual.folderId())
+    final var actual = new YandexCloudProperties("x", "y", "z");
+    assertThat(actual.translationUrl())
         .isEqualTo("x");
-    assertThat(actual.authorizationToken())
+    assertThat(actual.folderId())
         .isEqualTo("y");
+    assertThat(actual.authorizationToken())
+        .isEqualTo("z");
   }
 }
