a42e0cfa2f044b77442dce299ac19120fc7b8ecd
Rinat Zalyaletdinov <rinat.zalyaletdinov@yandex.com>
2022-11-11 21:46:21 +0300

    Introduce new type of tests - smoke tests
    Common annotations extracted to new RestClientSmokeTest annotation.
    DirtiesContext annotation used for destroying application context.
    There was an issue when mocked server was called by IamTokenKeeper
    after test completion due to scheduling. When DirtiesContext used
    scheduler destroyed with application context.

---
+++ src/test/java/tatbash/infrastructure/smoketest/RestClientSmokeTest.java
@@ -0,0 +1,38 @@
+package tatbash.infrastructure.smoketest;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * <p>
+ * {@link RestClientTest} annotation used for testing and mocking {@link RestTemplate} requests.
+ * </p>
+ * <p>
+ * See <a href="https://rieckpil.de/testing-your-spring-resttemplate-with-restclienttest/">an article</a>.
+ * </p>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles({"smoke-test"})
+@RestClientTest
+@TestPropertySource
+public @interface RestClientSmokeTest {
+
+  /**
+   * @see RestClientTest#value()
+   */
+  @AliasFor(annotation = RestClientTest.class, attribute = "value")
+  Class<?>[] value() default {};
+
+  /**
+   * @see TestPropertySource#properties()
+   */
+  @AliasFor(annotation = TestPropertySource.class, attribute = "properties")
+  String[] properties() default {};
+}
